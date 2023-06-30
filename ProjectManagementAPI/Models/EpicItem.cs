d65e5aa2168e31db2a9df9501a72dfeb2fabadf9
simonvignes <simon.vignes@outlook.com>
2023-06-30 09:08:58 +0200

    Changed default value of data types

--- ProjectManagementAPI/Models/EpicItem.cs
+++ ProjectManagementAPI/Models/EpicItem.cs
@@ -4,9 +4,9 @@ public class EpicItem
 {
     public int Epic_id { get; set; }
 
-    public string Name { get; set; } = String.Empty;
+    public string? Name { get; set; }
 
-    public string Description { get; set; } = String.Empty;
+    public string? Description { get; set; }
 
     public int Project_id { get; set; }
 
